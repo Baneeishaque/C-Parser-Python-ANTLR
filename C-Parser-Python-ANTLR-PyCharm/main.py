f2b795d227e467c3ddc9904d69433d8482cc1391
Banee Ishaque K <baneeishaque@gmail.com>
2024-03-13 16:02:27 +0530

    Function Info

--- C-Parser-Python-ANTLR-PyCharm/main.py
+++ C-Parser-Python-ANTLR-PyCharm/main.py
@@ -7,7 +7,13 @@ from CParser import CParser
 
 class FunctionListener(CListener):
     def enterFunctionDefinition(self, ctx: CParser.FunctionDefinitionContext):
-        print("Found a function: " + ctx.getText())
+        # Get the function name
+        function_name = ctx.declarator().directDeclarator().directDeclarator().getText()
+
+        # Get the return type
+        return_type = ctx.declarationSpecifiers().getText()
+
+        print(f"Function Name: {function_name}, Return Type: {return_type}")
 
 
 def main():
